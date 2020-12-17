89d10a371ad6c6eaa069bc2c564655339ee14285
Kyle Melton <kalm42@gmail.com>
2020-12-17 08:51:20 -0800

    Add resume page

---
+++ content/resume.md
@@ -0,0 +1,45 @@
+---
+templateKey: resume-page
+title: Resume
+workExperience:
+  - company: A company
+    jobTitle: Job Title
+    startDate: 2018-06-19
+    endDate: 2020-06-19
+    jobDescription: Long form job description.
+  - company: A company
+    jobTitle: Job Title
+    startDate: 2018-06-19
+    endDate: 2020-06-19
+    jobDescription: Long form job description.
+  - company: A company
+    jobTitle: Job Title
+    startDate: 2018-06-19
+    endDate: 2020-06-19
+    jobDescription: Long form job description.
+education:
+  - college: CBU
+    degree: Accounting
+    graduationDate: 2020-06-19
+  - college: CBU
+    degree: Accounting
+    graduationDate: 2020-06-19
+certifications:
+  - title: Google Mobile Web Developer
+    description: A long form description of the certificate.
+  - title: Google Mobile Web Developer
+    description: A long form description of the certificate.
+projects:
+  - title: Project Title
+    description: A long form description of the project
+    tags:
+      - tag: The Tag
+  - title: Project Title
+    description: A long form description of the project
+    tags:
+      - tag: The Tag
+skills:
+  - skill: The Skill
+  - skill: The Skill
+  - skill: The Skill
+---
--- src/templates/resume-page.js
+++ src/templates/resume-page.js
@@ -1,68 +1,146 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import styles from "./ResumePage.module.css"
+import { format } from "date-fns"
 
-const r = () => {
+export const ResumePageTemplate = props => {
+  const {
+    title,
+    workExperience,
+    education,
+    certifications,
+    projects,
+    skills,
+  } = props
   return (
-    <section>
-      <section>
-        <h1>Resume</h1>
-
+    <div className={styles["ResumePage__root"]}>
+      <h1 className={styles["ResumePage__title"]}>{title}</h1>
+      <article>
         <section>
-          <h3>Work Experience</h3>
-          <section>
-            <h5>COMPANY - JOB TITLE</h5>
-            <p>START DATE - END DATE | Present</p>
-            <p>JOB DESCRIPTION</p>
-          </section>
+          <h2>Work Experience</h2>
+          {workExperience.map(
+            ({ company, jobTitle, startDate, endDate, jobDescription }, i) => (
+              <article key={i} className={styles["ResumePage__item"]}>
+                <h3>
+                  {company} — {jobTitle}
+                </h3>
+                <p>
+                  <small>
+                    {format(new Date(startDate), "MM/dd/yyyy")} –{" "}
+                    {format(new Date(endDate), "MM/dd/yyyy")}
+                  </small>
+                </p>
+                {jobDescription}
+              </article>
+            )
+          )}
         </section>
-
         <section>
-          <h3>Eduction</h3>
-          <section>
-            <h5>COLLEGE</h5>
-            <p>DEGREE DETAILS</p>
-          </section>
+          <h2>Education</h2>
+          {education.map(({ college, degree, graduationDate }, i) => (
+            <article key={i} className={styles["ResumePage__item"]}>
+              <h3>{college}</h3>
+              <p>
+                {degree} - {format(new Date(graduationDate), "MM/yyyy")}
+              </p>
+            </article>
+          ))}
         </section>
-
         <section>
-          <h3>Certifications</h3>
-          <section>
-            <h5>CERT TITLE</h5>
-            <p>CERT DESCRIPTION</p>
-          </section>
+          <h2>Projects</h2>
+          {projects.map(({ title, description, tags }, i) => (
+            <article kei={i} className={styles["ResumePage__item"]}>
+              <h3>{title}</h3>
+              {description}
+              <ul>
+                {tags.map(({ tag }, j) => (
+                  <li key={j}>{tag}</li>
+                ))}
+              </ul>
+            </article>
+          ))}
         </section>
-      </section>
-
-      <section>
-        <h3>Projects</h3>
-        <section>
-          <section>
-            <h5>PROJECT TITLE</h5>
-            <p>PROJECT DESCRIPTION</p>
-            <h6>Tags</h6>
-            <ul>
-              <li>tag</li>
-            </ul>
-          </section>
-        </section>
-      </section>
-
+      </article>
       <aside>
-        <h3>Skills</h3>
         <section>
-          <h5>Languages</h5>
-          <ul>
-            <li>JavaScript</li>
-          </ul>
+          <h2>Certifications</h2>
+          {certifications.map(({ title, description }, i) => (
+            <article key={i}>
+              <h3>{title}</h3>
+              {description}
+            </article>
+          ))}
         </section>
         <section>
-          <h5>Tools</h5>
+          <h2>Skills</h2>
           <ul>
-            <li>Git</li>
+            {skills.map(({ skill }, i) => (
+              <li key={i}>{skill}</li>
+            ))}
           </ul>
         </section>
       </aside>
-    </section>
+    </div>
   )
 }
 
-export default r
+export default ({ data }) => {
+  const {
+    title,
+    workExperience,
+    education,
+    certifications,
+    projects,
+    skills,
+  } = data.markdownRemark.frontmatter
+
+  return (
+    <Layout>
+      <ResumePageTemplate
+        title={title}
+        workExperience={workExperience}
+        education={education}
+        certifications={certifications}
+        projects={projects}
+        skills={skills}
+      />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query ResumePageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "resume-page" } }) {
+      frontmatter {
+        title
+        workExperience {
+          company
+          jobTitle
+          startDate
+          endDate
+          jobDescription
+        }
+        education {
+          college
+          degree
+          graduationDate
+        }
+        certifications {
+          title
+          description
+        }
+        projects {
+          title
+          description
+          tags {
+            tag
+          }
+        }
+        skills {
+          skill
+        }
+      }
+    }
+  }
+`
